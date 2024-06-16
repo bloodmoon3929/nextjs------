19b19272239ba6ac9acc32655d016ed6d5a4a41d
KimSangWoo <gnbupi@naver.com>
2024-06-16 23:25:40 +0900

    default

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,9 +1,10 @@
+import Layout from "../components/layout";
 
 export default function AboutMe(){
     return (
-        <div>
+        <Layout>
             <h1>dddd</h1>
-        </div>
+        </Layout>
 
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Layout from "../components/layout";
+import Head from 'next/head'
+import Image from "next/image";
+
 
 export default function Home() {
     return(
         <Layout>
+            <Head>
+                <title>test</title>
+                <meta name="description" content="testtest"></meta>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
             
-            <h1>홈</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
 
 
         </Layout>
